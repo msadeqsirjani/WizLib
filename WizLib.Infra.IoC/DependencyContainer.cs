c08a2e9e3772f3ff3c4b62f08e619fb14685dc51
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-20 14:20:31 +0330

    Create DependencyContainer.cs

---
+++ WizLib.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WizLib.Infra.Data.Persistence;
+
+namespace WizLib.Infra.IoC
+{
+    public static class DependencyContainer
+    {
+        public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddDistributedMemoryCache()
+                .AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+
+        }
+
+        public static void EnableMiddleWares(this IApplicationBuilder app, IWebHostEnvironment env,
+            IConfiguration configuration)
+        {
+
+        }
+    }
+}
