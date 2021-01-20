20a9853e1b0ab142da8a8b70c0584d1d9908c220
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-20 14:20:28 +0330

    Create ApplicationDbContext.cs

---
+++ WizLiab.Infra.Data/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WizLib.Infra.Data.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
