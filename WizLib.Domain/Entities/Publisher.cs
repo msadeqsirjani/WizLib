3d1e1b727bd73f9f41a565dd01204dbaaa8629f9
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-21 10:20:00 +0330

    Create Publisher.cs
    add publisher entity

---
+++ WizLib.Domain/Entities/Publisher.cs
@@ -0,0 +1,10 @@
+﻿using WizLib.Domain.Common;
+
+namespace WizLib.Domain.Entities
+{
+    public class Publisher : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
