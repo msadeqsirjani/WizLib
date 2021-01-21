17b1b59bf03ef74d79966e4cb4a638581caf7c9d
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-21 10:03:44 +0330

    Add Migration
    add book poco class to wizlib db

---
+++ WizLiab.Infra.Data/Persistence/Configuration/BookConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WizLib.Domain.Entities;
+using WizLib.Infra.Data.Persistence.Configuration.Common;
+
+namespace WizLib.Infra.Data.Persistence.Configuration
+{
+    public class BookConfiguration : BaseEntityConfiguration<Book>
+    {
+        public override void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.Property(x => x.Title)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.Isbn)
+                .IsRequired()
+                .HasMaxLength(16)
+                .HasColumnName("ISBN");
+
+            builder.Property(x => x.Price)
+                .IsRequired()
+                .HasMaxLength(3000000);
+
+            base.Configure(builder);
+        }
+    }
+}
