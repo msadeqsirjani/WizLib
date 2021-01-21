4377fc64b3304f0a127eac9a570ab9890bd822c2
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-21 11:44:00 +0330

    Update Category.cs
    add relation between book & category

--- WizLib.Domain/Entities/Category.cs
+++ WizLib.Domain/Entities/Category.cs
@@ -1,9 +1,12 @@
-﻿using WizLib.Domain.Common;
+﻿using System.Collections.Generic;
+using WizLib.Domain.Common;
 
 namespace WizLib.Domain.Entities
 {
     public class Category : BaseEntity
     {
         public string Title { get; set; }
+
+        public List<Book> Books { get; set; }
     }
 }
