353147a439e3c97c8f212b84239a4fb0971fa8c5
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-21 10:19:50 +0330

    Create Author.cs
    add author entity

---
+++ WizLib.Domain/Entities/Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using WizLib.Domain.Common;
+
+namespace WizLib.Domain.Entities
+{
+    public class Author : BaseEntity
+    {
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Location { get; set; }
+
+    }
+}
