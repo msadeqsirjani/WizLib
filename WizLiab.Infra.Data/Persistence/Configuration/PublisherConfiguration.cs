314f3cc4dc57b6c8da45ceff79fa36b226c2dcb9
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-21 10:25:55 +0330

    Create PublisherConfiguration.cs
    apply publisher configuration

---
+++ WizLiab.Infra.Data/Persistence/Configuration/PublisherConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WizLib.Domain.Entities;
+using WizLib.Infra.Data.Persistence.Configuration.Common;
+
+namespace WizLib.Infra.Data.Persistence.Configuration
+{
+    public class PublisherConfiguration : BaseEntityConfiguration<Publisher>
+    {
+        public override void Configure(EntityTypeBuilder<Publisher> builder)
+        {
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.Location)
+                .IsRequired()
+                .HasMaxLength(64);
+
+            base.Configure(builder);
+        }
+    }
+}
