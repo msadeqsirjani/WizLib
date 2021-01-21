d4bb69434ae9d817bb37fc852d40379a683483ce
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-21 10:25:43 +0330

    Create AuthorConfiguration.cs
    apply author configuration

---
+++ WizLiab.Infra.Data/Persistence/Configuration/AuthorConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WizLib.Domain.Entities;
+using WizLib.Infra.Data.Persistence.Configuration.Common;
+
+namespace WizLib.Infra.Data.Persistence.Configuration
+{
+    public class AuthorConfiguration : BaseEntityConfiguration<Author>
+    {
+        public override void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.Property(x => x.Forename)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.Surname)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.Birthdate)
+                .IsRequired();
+
+            builder.Property(x => x.Location)
+                .IsRequired()
+                .HasMaxLength(64);
+
+            base.Configure(builder);
+        }
+    }
+}
