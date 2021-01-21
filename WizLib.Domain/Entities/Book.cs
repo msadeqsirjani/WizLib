04cb825e0590c1e3fe11b50ce1bab829154aa8a2
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-21 10:02:55 +0330

    Create Book.cs
    add book poco entity

---
+++ WizLib.Domain/Entities/Book.cs
@@ -0,0 +1,11 @@
+﻿using WizLib.Domain.Common;
+
+namespace WizLib.Domain.Entities
+{
+    public class Book : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Isbn { get; set; }
+        public double Price { get; set; }
+    }
+}
