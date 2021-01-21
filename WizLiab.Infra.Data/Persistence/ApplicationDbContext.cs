0e30d09a8b379623853eb39554dcf5231f09a477
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-21 10:26:15 +0330

    Update ApplicationDbContext.cs
    apply author, publisher configuration to database

--- WizLiab.Infra.Data/Persistence/ApplicationDbContext.cs
+++ WizLiab.Infra.Data/Persistence/ApplicationDbContext.cs
@@ -14,12 +14,16 @@ namespace WizLib.Infra.Data.Persistence
         public DbSet<Category> Categories { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.ApplyConfiguration(new CategoryConfiguration());
             builder.ApplyConfiguration(new GenreConfiguration());
             builder.ApplyConfiguration(new BookConfiguration());
+            builder.ApplyConfiguration(new AuthorConfiguration());
+            builder.ApplyConfiguration(new PublisherConfiguration())
 
             base.OnModelCreating(builder);
 
