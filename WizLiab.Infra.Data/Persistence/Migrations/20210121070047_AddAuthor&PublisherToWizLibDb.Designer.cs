2f853823dcbb6eed88e314019de773e29ebe5970
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-21 10:46:07 +0330

    Add Migration
    add author, publisher

--- WizLiab.Infra.Data/Persistence/Migrations/20210121070047_AddAuthor&PublisherToWizLibDb.Designer.cs
+++ WizLiab.Infra.Data/Persistence/Migrations/20210121070047_AddAuthor&PublisherToWizLibDb.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WizLib.Infra.Data.Persistence;
 
 namespace WizLib.Infra.Data.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210121070047_AddAuthor&PublisherToWizLibDb")]
+    partial class AddAuthorPublisherToWizLibDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
