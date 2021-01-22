bf781a8a00aa4ae6db2fa39632625ee8d9a61bc8
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-22 13:26:07 +0330

    Create PublisherController.cs
    add publisher controller

---
+++ WizLib.Web/Controllers/PublisherController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WizLib.Domain.Entities;
+using WizLib.Infra.Data.Persistence;
+
+namespace WizLib.Web.Controllers
+{
+    public class PublisherController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PublisherController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var publishers = _db.Publishers.ToList();
+
+            return View(publishers);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            if (id.HasValue)
+            {
+                var publisher = _db.Publishers.FirstOrDefault(x => x.Id == id);
+
+                return View(publisher);
+            }
+            else
+            {
+                var publisher = new Publisher();
+
+                return View(publisher);
+            }
+        }
+    }
+}
