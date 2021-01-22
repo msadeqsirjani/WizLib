56615b959cabdc12221f83862cbf611f1c55e47e
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-22 13:51:01 +0330

    Update PublisherController.cs
    publisher controller is finally complete

--- WizLib.Web/Controllers/PublisherController.cs
+++ WizLib.Web/Controllers/PublisherController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 using WizLib.Domain.Entities;
 using WizLib.Infra.Data.Persistence;
@@ -25,16 +26,56 @@ namespace WizLib.Web.Controllers
         {
             if (id.HasValue)
             {
-                var publisher = _db.Publishers.FirstOrDefault(x => x.Id == id);
+                var publisher = _db.Publishers.FirstOrDefault(x => x.Id == id.GetValueOrDefault());
+
+                if (publisher == null)
+                    return NotFound();
 
                 return View(publisher);
             }
             else
             {
-                var publisher = new Publisher();
+                var publisher = new Publisher
+                {
+                    Name = Guid.NewGuid().ToString(),
+                    Location = Guid.NewGuid().ToString()
+                };
 
                 return View(publisher);
             }
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Publisher publisher)
+        {
+            if (!ModelState.IsValid)
+                return View(publisher);
+
+            if (publisher.Id == 0)
+            {
+                _db.Publishers.Add(publisher);
+            }
+            else
+            {
+                _db.Update(publisher);
+            }
+
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var publisher = _db.Publishers.FirstOrDefault(x => x.Id == id);
+
+            if (publisher == null)
+                return NotFound();
+
+            _db.Publishers.Remove(publisher);
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
