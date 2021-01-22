52ca9c5d5f58c7b7d8ffa4a9e0794f006dfda67b
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-22 12:56:08 +0330

    Update CategoryController.cs
    add upsert, create multiple 2, create multiple 5, remove multiple 2, remove multiple 5 action

--- WizLib.Web/Controllers/CategoryController.cs
+++ WizLib.Web/Controllers/CategoryController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using WizLib.Domain.Entities;
 using WizLib.Infra.Data.Persistence;
 
 namespace WizLib.Web.Controllers
@@ -18,6 +21,122 @@ namespace WizLib.Web.Controllers
             var categories = _db.Categories.ToList();
 
             return View(categories);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            if (id.HasValue)
+            {
+                var category = _db.Categories.FirstOrDefault(x => x.Id == id.GetValueOrDefault());
+
+                if (category == null)
+                    return NotFound();
+
+                return View(category);
+            }
+            else
+            {
+                var category = new Category();
+
+                return View(category);
+            }
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (!ModelState.IsValid)
+                return View(category);
+
+            if (category.Id == 0)
+            {
+                _db.Categories.Add(category);
+            }
+            else
+            {
+                _db.Categories.Update(category);
+            }
+
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (!id.HasValue || id.Value == 0)
+                return NotFound();
+
+            var category = _db.Categories.FirstOrDefault(x => x.Id == id.GetValueOrDefault());
+
+            if (category == null)
+                return NotFound();
+
+            _db.Categories.Remove(category);
+
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult CreateMultiple2()
+        {
+            var categories = new List<Category>();
+
+            for (var i = 0; i < 2; i++)
+            {
+                categories.Add(new Category
+                {
+                    Title = Guid.NewGuid().ToString()
+                });
+            }
+
+            _db.AddRange(categories);
+
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult CreateMultiple5()
+        {
+            var categories = new List<Category>();
+
+            for (var i = 0; i < 5; i++)
+            {
+                categories.Add(new Category
+                {
+                    Title = Guid.NewGuid().ToString()
+                });
+            }
+
+            _db.AddRange(categories);
+
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult RemoveMultiple2()
+        {
+            var categories = _db.Categories.OrderBy(x => Guid.NewGuid()).Take(2);
+
+            _db.Categories.RemoveRange(categories);
+
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult RemoveMultiple5()
+        {
+            var categories = _db.Categories.OrderBy(x => Guid.NewGuid()).Take(5);
+
+            _db.Categories.RemoveRange(categories);
+
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
