142a57c7a5690794ccd2ac804aaac5480f31cc22
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-22 12:56:18 +0330

    Update Category.cs
    add validation on category

--- WizLib.Domain/Entities/Category.cs
+++ WizLib.Domain/Entities/Category.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using WizLib.Domain.Common;
 
 namespace WizLib.Domain.Entities
 {
     public class Category : BaseEntity
     {
+        [Required, StringLength(100, MinimumLength = 2)]
         public string Title { get; set; }
 
         public List<Book> Books { get; set; }
