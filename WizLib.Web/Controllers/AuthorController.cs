6f60b948123a6a41787dc5a5f8b104d9077e3deb
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-22 14:08:38 +0330

    Update AuthorController.cs

--- WizLib.Web/Controllers/AuthorController.cs
+++ WizLib.Web/Controllers/AuthorController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using WizLib.Domain.Entities;
 using WizLib.Infra.Data.Persistence;
@@ -34,9 +35,9 @@ namespace WizLib.Web.Controllers
             }
             else
             {
-                var random = new Random(DateTime.Now.Ticks.ToInt32());
+                var random = new Random();
 
-                var days = random.Next(-16384, 16384);
+                var days = random.Next(-16384 * 2, 0);
 
                 var author = new Author
                 {
@@ -48,6 +49,110 @@ namespace WizLib.Web.Controllers
 
                 return View(author);
             }
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Author author)
+        {
+            if (!ModelState.IsValid)
+                return View(author);
+
+            if (author.Id.IsDefault())
+            {
+                _db.Authors.Add(author);
+            }
+            else
+            {
+                _db.Authors.Update(author);
+            }
+
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var author = _db.Authors.FirstOrDefault(x => x.Id == id);
+
+            if (author == null)
+                return NotFound();
+
+            _db.Authors.Remove(author);
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult CreateMultiple2()
+        {
+            var authors = new List<Author>();
+
+            var random = new Random();
+            for (var i = 0; i < 2; i++)
+            {
+                var days = random.Next(-16384 * 2, 0);
+
+                authors.Add(new Author()
+                {
+                    Forename = Guid.NewGuid().ToString(),
+                    Surname = Guid.NewGuid().ToString(),
+                    Location = Guid.NewGuid().ToString(),
+                    Birthdate = DateTime.Now.AddDays(days)
+                });
+            }
+
+            _db.Authors.AddRange(authors);
+
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult CreateMultiple5()
+        {
+            var authors = new List<Author>();
+
+            var random = new Random();
+            for (var i = 0; i < 5; i++)
+            {
+                var days = random.Next(-16384 * 2, 0);
+                authors.Add(new Author()
+                {
+                    Forename = Guid.NewGuid().ToString(),
+                    Surname = Guid.NewGuid().ToString(),
+                    Location = Guid.NewGuid().ToString(),
+                    Birthdate = DateTime.Now.AddDays(days)
+                });
+            }
+
+            _db.Authors.AddRange(authors);
+
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult RemoveMultiple2()
+        {
+            var authors = _db.Authors.OrderBy(x => Guid.NewGuid()).Take(2);
+
+            _db.Authors.RemoveRange(authors);
+
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult RemoveMultiple5()
+        {
+            var authors = _db.Authors.OrderBy(x => Guid.NewGuid()).Take(5);
+
+            _db.Authors.RemoveRange(authors);
+
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
