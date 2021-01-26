8fd1fe7786a904194222fc5cf81e1411b4d6abf1
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-26 09:34:00 +0330

    Add Migration

--- WizLiab.Infra.Data/Persistence/Migrations/20210126060318_MakeBookDetailIdNullable.Designer.cs
+++ WizLiab.Infra.Data/Persistence/Migrations/20210126060318_MakeBookDetailIdNullable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WizLib.Infra.Data.Persistence;
 
 namespace WizLib.Infra.Data.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210126060318_MakeBookDetailIdNullable")]
+    partial class MakeBookDetailIdNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
