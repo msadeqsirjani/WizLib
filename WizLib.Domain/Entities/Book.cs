9462be541129b8c83848c5455422640d2517f95a
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-26 09:32:32 +0330

    Update Book.cs

--- WizLib.Domain/Entities/Book.cs
+++ WizLib.Domain/Entities/Book.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using WizLib.Domain.Common;
 
 namespace WizLib.Domain.Entities
 {
     public class Book : BaseEntity
     {
+        [Required, StringLength(100, MinimumLength = 2)]
         public string Title { get; set; }
+        [Required, StringLength(16, MinimumLength = 4)]
         public string Isbn { get; set; }
         public double Price { get; set; }
         public int CategoryId { get; set; }
-        public int BookDetailId { get; set; }
+        public int? BookDetailId { get; set; }
         public int PublisherId { get; set; }
 
         public Category Category { get; set; }
