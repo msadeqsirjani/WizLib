544bb8491ff091489c71f837aa033d5f3ea2ce80
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-02-01 21:06:39 +0330

    Create AuthorBookVm.cs

---
+++ WizLib.Application/ViewModels/AuthorBookVm.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using WizLib.Domain.Entities;
+
+namespace WizLib.Application.ViewModels
+{
+    public class AuthorBookVm
+    {
+        public AuthorBook AuthorBook { get; set; }
+        public Book Book { get; set; }
+        public IEnumerable<AuthorBook> AuthorBooks { get; set; }
+        public IEnumerable<SelectListItem> Authors { get; set; }
+    }
+}
