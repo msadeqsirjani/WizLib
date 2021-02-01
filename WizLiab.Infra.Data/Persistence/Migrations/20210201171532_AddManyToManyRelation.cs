32039d24f0057d51c1fe0984b46439edfa038a63
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-02-01 21:06:09 +0330

    Create 20210201171532_AddManyToManyRelation.cs

---
+++ WizLiab.Infra.Data/Persistence/Migrations/20210201171532_AddManyToManyRelation.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WizLib.Infra.Data.Persistence.Migrations
+{
+    public partial class AddManyToManyRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
