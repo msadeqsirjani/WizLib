52b094e513d8debb4b13e39f065d6c9628d2ca12
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-02-01 14:22:43 +0330

    Create CategoryInitializer.cs

---
+++ WizLib.Application/Seed/CategoryInitializer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace WizLib.Application.Seed
+{
+    public class CategoryInitializer
+    {
+        private static readonly string[] Categories =
+        {
+            "Fantasy",
+            "Adventure",
+            "Romance",
+            "Contemporary",
+            "Dystopian",
+            "Mystery",
+            "Horror",
+            "Thriller",
+            "Paranormal",
+            "Historical fiction",
+            "Science Fiction",
+            "Memoir",
+            "Cooking",
+            "Art",
+            "Self-help / Personal",
+            "Development",
+            "Motivational",
+            "Health",
+            "History",
+            "Travel",
+            "Guide / How-to",
+            "Families & Relationships",
+            "Humor",
+            "Children’s"
+        };
+
+        private static readonly int Length = Categories.Length;
+
+        public static string Seed()
+        {
+            var random = new Random();
+
+            return $"{Categories[random.Next(Length)]} {Guid.NewGuid().ToString()[..6]}";
+        }
+    }
+}
