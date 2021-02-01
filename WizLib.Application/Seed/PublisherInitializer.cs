b3b473673834fd7031f753125ad6f31794b7ba1f
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-02-01 14:22:52 +0330

    Create PublisherInitializer.cs

---
+++ WizLib.Application/Seed/PublisherInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WizLib.Application.Seed
+{
+    public class PublisherInitializer
+    {
+        private static readonly string[] Publishers =
+        {
+            "Penguin Random House",
+            "Hachette Livre",
+            "HarperCollins",
+            "Macmillan Publishers",
+            "Simon & Schuster",
+            "McGraw-Hill Education",
+            "Houghton Mifflin Harcourt",
+            "Pearson Education",
+            "Scholastic",
+            "Cengage Learning",
+            "Springer Nature",
+            "Wiley (John Wiley & Sons)",
+            "Oxford University Press",
+            "Kodansha",
+            "Shueisha",
+            "Grupo Santillana",
+            "Bonnier Books",
+            "Editis",
+            "Klett",
+            "Egmont Books"
+        };
+
+        private static readonly int Length = Publishers.Length;
+
+        public static string Seed()
+        {
+            var random = new Random();
+
+            return $"{Publishers[random.Next(Length)]} {Guid.NewGuid().ToString()[..6]}";
+        }
+    }
+}
